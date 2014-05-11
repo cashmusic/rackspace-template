80ad1eacf1c1cd2bc0175973476c1a06041e8ef5
Jeff Sheltren <jeff@tag1consulting.com>
2014-05-10 17:49:00 -0700

    Merge default node into site.pp No need for the nodes directory, class definitions per-node will be defined in hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,3 +8,23 @@ filebucket { main: server => puppet }
 # global defaults
 File { backup => main }
 Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
+
+node default {
+  stage { 'pre': before => Stage['main'] }
+  class { 'yumrepos::epel': stage => 'pre' }
+  class { 'yumrepos::ius': stage => 'pre' }
+
+
+  # Firewall setup
+  resources { "firewall":
+    purge => true
+  }
+  class { 'firewall': }
+  Firewall {
+    before  => Class['site_firewall::post'],
+    require => Class['site_firewall::pre'],
+  }
+  class { ['site_firewall::pre', 'site_firewall::post']: }
+
+
+}
