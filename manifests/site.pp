0c9a39f9c791bd0948965a66c45b22874b15b5ba
Jeff Sheltren <jeff@tag1consulting.com>
2014-05-10 17:59:41 -0700

    Add hostenv/hosttype definitions to site.pp -- to be used in hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,15 @@
 # site.pp
 
+# Pull "hostenv" (a.k.a environment) and "type" out of fqdn
+# to be used by Hiera.
+# Hostname examples:
+# prod-web1.cashmusic.org => hostenv: prod; hosttype: web
+# stage-db1.cashmusic.org => hostenv: stage; hosttype: db
+# dev-multi1.cashmusic.org => hostenv: stage; hosttype: multi
+# vagrant-multi1.dev => hostenv: vagrant; hosttype: multi
+$hostenv = regsubst($fqdn, '^([a-z]+)-([a-z]+)(\d)\.(.*)$', '\1')
+$hosttype = regsubst($fqdn, '^([a-z]+)-([a-z]+)(\d)\.(.*)$', '\2')
+
 hiera_include('classes')
 import "nodes/*.pp"
 
