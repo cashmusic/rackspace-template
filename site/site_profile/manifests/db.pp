9ae98539ed71cd2e0eb372744fa13b4ed95fbe43
Jeff Sheltren <jeff@tag1consulting.com>
2014-05-25 08:29:34 -0700

    merge mysqld options

--- site/site_profile/manifests/db.pp
+++ site/site_profile/manifests/db.pp
@@ -9,7 +9,9 @@ class site_profile::db {
   # dbclient class handles mysql->mysql55 client/libs packages.  
   require site_profile::dbclient
 
-  class { 'mysql::server': }
+  # Need to use hiera_hash to get the right mysql options (so we can merge hashes from multiple configs).
+  $mysql_options = hiera_hash('site_profile::db::mysql_options', {})
+  class { 'mysql::server': override_options => $mysql_options }
 
   # Need to run the dbclient class first before installing the server
   # due to packaging conflicts between base mysql and IUS mysql.
