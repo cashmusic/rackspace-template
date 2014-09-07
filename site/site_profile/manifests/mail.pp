7f3d18287359a39dbee89b81f92e517887844d0e
Jeff Sheltren <jeff@tag1consulting.com>
2014-09-06 17:02:04 -0700

    Manage /etc/aliases in mail class

--- site/site_profile/manifests/mail.pp
+++ site/site_profile/manifests/mail.pp
@@ -15,6 +15,21 @@ class site_profile::mail {
       content => template("site_profile/etc/postfix/sasl_passwd.erb");
   }
 
+  # Alias configuration.
+  $alias_entries = hiera_hash('site_profile::mail::alias_entries', { })
+  file { "/etc/aliases":
+    content => template("site_profile/etc/aliases.erb"),
+    owner => root,
+    group => root,
+    mode => 644,
+  }
+
+  exec { "newaliases":
+    command => "/usr/bin/newaliases",
+    subscribe => File["/etc/aliases"],
+    refreshonly => true,
+  }
+
   # All postfix configuration is in hiera.
   class { 'postfix::server': }
 
